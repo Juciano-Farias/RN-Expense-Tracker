95f16fac26854bf002a2df54065655409474f108
Juciano <juciano.junior@hotmail.com>
2022-07-25 12:13:26 +0100

    feat: expenses item for FlatList

---
+++ components/ExpensesOutput/ExpenseItem.js
@@ -0,0 +1,62 @@
+import { Pressable, Text, View, StyleSheet } from "react-native";
+
+import { GlobalStyles } from "../../constants/styles";
+
+import { getFormattedDate } from '../../util/date'
+
+const ExpenseItem = ({ description, amount, date }) => {
+  return (
+    <Pressable>
+      <View style={styles.expenseItem}>
+        <View>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amountText}>{amount.toFixed(2)}</Text>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
+
+export default ExpenseItem;
+
+const styles = StyleSheet.create({
+  expenseItem: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 6,
+    elevation: 3,
+    shadowColor: GlobalStyles.colors.primary500,
+    shadowRadius: 4,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.4,
+  },
+  textBase: {
+    color: GlobalStyles.colors.primary50,
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+  amountContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    borderRadius: 4,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    minWidth: 80,
+  },
+  amountText: {
+    color: GlobalStyles.colors.primary500,
+    fontWeight: "bold",
+  },
+});
