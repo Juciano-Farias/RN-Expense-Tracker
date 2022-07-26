f6b6b46bacfd46de830ca852254dbd142ad04877
Juciano <juciano.junior@hotmail.com>
2022-07-26 14:59:37 +0100

    feat: styling the manage page

--- screen/ManageExpenses.js
+++ screen/ManageExpenses.js
@@ -1,9 +1,52 @@
-import { Text } from "react-native"
+import { useLayoutEffect } from "react";
+import { StyleSheet, View } from "react-native";
 
-const ManageExpenses = () => {
+import IconButton from "../components/UI/IconsButton";
+import { GlobalStyles } from "../constants/styles";
+
+const ManageExpenses = ({ route, navigation }) => {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHandler() {
+
+  }
+
   return (
-    <Text>Recent Expenses</Text>
-  )
-}
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            size={36}
+            color={GlobalStyles.colors.error500}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
+};
 
-export default ManageExpenses
+export default ManageExpenses;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    padding: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+})
