fe0c1f68a10c32be1a9be7af934b1f1168752807
Juciano <juciano.junior@hotmail.com>
2022-07-29 00:58:22 +0100

    feat: default values

--- screen/ManageExpenses.js
+++ screen/ManageExpenses.js
@@ -13,6 +13,10 @@ const ManageExpenses = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
+  const selectedExpense = expensesContext.expenses.find(
+    (expense) => expense.id === editedExpenseId
+  );
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: isEditing ? "Edit Expense" : "Add Expense",
@@ -43,6 +47,7 @@ const ManageExpenses = ({ route, navigation }) => {
         submitButtonLabel={isEditing ? "Update" : "Add"}
         onCancel={cancelHandler}
         onSubmit={confirmHandler}
+        defaultValue={selectedExpense}
       />
 
       {isEditing && (
