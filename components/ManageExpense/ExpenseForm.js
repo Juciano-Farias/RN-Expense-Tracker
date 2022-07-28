3922188949a372919e4744734d512607bd3fa70a
Juciano <juciano.junior@hotmail.com>
2022-07-28 21:03:50 +0100

    feat: inputs props

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,14 +1,31 @@
-import { View } from "react-native"
-import Input from "./Input"
+import { View } from "react-native";
+import Input from "./Input";
 
 const ExpenseForm = () => {
+  function amountChangedHandler() {}
+
   return (
     <View>
-        <Input label="Amount"/>
-        <Input label="Date"/>
-        <Input label="Description"/>
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangedHandler,
+        }}
+      />
+      <Input
+        label="Date"
+        textInputConfig={{
+          placeholder: "YYY-MM-DD",
+          maxLneght: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input label="Description" textInputConfig={{
+        multiline: true,
+      }} />
     </View>
-  )
-}
+  );
+};
 
-export default ExpenseForm
+export default ExpenseForm;
