15ed74852b4e6e1041c62033272f21070288a26f
Juciano <juciano.junior@hotmail.com>
2022-07-29 09:38:16 +0100

    feat: fetching the expenses

--- screen/RecentExpenses.js
+++ screen/RecentExpenses.js
@@ -1,10 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../context/expenses-context";
 import { getDateMinusDay } from "../util/date";
+import { fetchExpenses } from '../util/http'
 
 const RecentExpenses = () => {
   const expensesContext = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses()
+      expensesContext.setExpenses(expenses)
+    }
+
+    getExpenses()
+  }, [])
 
   const recentExpenses = expensesContext.expenses.filter((expense) => {
     const today = new Date();
