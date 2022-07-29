6ef3c310aa8c63bc3a98cdab129560877bc85f9c
Juciano <juciano.junior@hotmail.com>
2022-07-29 09:03:52 +0100

    style: formarting the code

--- screen/RecentExpenses.js
+++ screen/RecentExpenses.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput"
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../context/expenses-context";
 import { getDateMinusDay } from "../util/date";
 
@@ -7,15 +7,19 @@ const RecentExpenses = () => {
   const expensesContext = useContext(ExpensesContext);
 
   const recentExpenses = expensesContext.expenses.filter((expense) => {
-    const today = new Date()
-    const dateSevenDaysAgo = getDateMinusDay(today, 7)
+    const today = new Date();
+    const dateSevenDaysAgo = getDateMinusDay(today, 7);
 
-    return expense.date > dateSevenDaysAgo
-  })
+    return expense.date > dateSevenDaysAgo;
+  });
 
   return (
-    <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 days" fallbackText="No expenses registered for the last 7 days."/>
-  )
-}
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 days"
+      fallbackText="No expenses registered for the last 7 days."
+    />
+  );
+};
 
-export default RecentExpenses
+export default RecentExpenses;
