4acfcf7ce89930c17288b4224e62e4a98cae412f
Juciano <juciano.junior@hotmail.com>
2022-07-29 01:29:29 +0100

    feat: switching the Alert for a Text error alert when the form is invalid

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -12,38 +12,62 @@ const ExpenseForm = ({
   onCancel,
   onSubmit,
 }) => {
-  const [inputValue, setInputValue] = useState({
-    amount: defaultValue ? defaultValue.amount.toString() : "",
-    date: defaultValue ? getFormattedDate(defaultValue.date) : "",
-    description: defaultValue ? defaultValue.description.toString() : "",
+  const [inputs, setInputs] = useState({
+    amount: {
+      value: defaultValue ? defaultValue.amount.toString() : "",
+      isValid: true,
+    },
+    date: {
+      value: defaultValue ? getFormattedDate(defaultValue.date) : "",
+      isValid: true,
+    },
+    description: {
+      value: defaultValue ? defaultValue.description.toString() : "",
+      isValid: true,
+    },
   });
   function inputChangeHandler(inputIdentifier, enteredValue) {
-    setInputValue((curInputValue) => {
+    setInputs((curInputs) => {
       return {
-        ...curInputValue,
-        [inputIdentifier]: enteredValue, // The square bracktes allow us to dynamically set the amount, date or description
+        ...curInputs,
+        [inputIdentifier]: { value: enteredValue, isValid: true }, // The square bracktes allow us to dynamically set the amount, date or description
       };
     });
   }
 
   function submitHandler() {
     const expenseData = {
-      amount: +inputValue.amount, // the + converts a string into a number
-      date: new Date(inputValue.date),
-      description: inputValue.description,
+      amount: +inputs.amount.value, // the + converts a string into a number
+      date: new Date(inputs.date.value),
+      description: inputs.description.value,
     };
 
     const amoutIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
     const dateIsValid = expenseData.date.toString() !== "Invalid Date";
-    const description = expenseData.description.trim().length > 0; // trim() ignores the excess spaces in the end and the begnning of the string
+    const descriptionIsValid = expenseData.description.trim().length > 0; // trim() ignores the excess spaces in the end and the begnning of the string
 
-    if (!amoutIsValid || !dateIsValid || !description) {
-      Alert.alert('Invalid input', 'Please check your input values')
+    if (!amoutIsValid || !dateIsValid || !descriptionIsValid) {
+      // Alert.alert("Invalid input", "Please check your input values");
+      setInputs((curInputs) => {
+        return {
+          amount: { value: curInputs.amount.value, isValid: amoutIsValid },
+          date: { value: curInputs.date.value, isValid: dateIsValid },
+          description: { 
+            value: curInputs.description.value,
+            isValid: descriptionIsValid,
+          },
+        };
+      });
       return;
     }
 
     onSubmit(expenseData);
   }
+
+  const formIsInvalid =
+    !inputs.amount.isValid ||
+    !inputs.date.isValid ||
+    !inputs.description.isValid
 
   return (
     <View style={styles.form}>
@@ -55,7 +79,7 @@ const ExpenseForm = ({
           textInputConfig={{
             keyboardType: "decimal-pad",
             onChangeText: inputChangeHandler.bind(this, "amount"),
-            value: inputValue.amount,
+            value: inputs.amount.value,
           }}
         />
         <Input
@@ -65,7 +89,7 @@ const ExpenseForm = ({
             placeholder: "YYY-MM-DD",
             maxLneght: 10,
             onChangeText: inputChangeHandler.bind(this, "date"),
-            value: inputValue.date,
+            value: inputs.date.value,
           }}
         />
       </View>
@@ -74,9 +98,10 @@ const ExpenseForm = ({
         textInputConfig={{
           multiline: true,
           onChangeText: inputChangeHandler.bind(this, "description"),
-          value: inputValue.description,
+          value: inputs.description.value,
         }}
       />
+      { formIsInvalid && <Text>Invalid input values - please check your input data</Text>}
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={onCancel}>
           Cancel
